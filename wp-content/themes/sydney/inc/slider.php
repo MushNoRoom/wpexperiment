4ff5c9740b96c1c0af4b42db3f3e908d2b8ec348
Bryan <fzboybryan@gmail.com>
2015-05-31 23:25:35 -0400

    Drowdown box with HTML forms. Ready for database operations

--- wp-content/themes/sydney/inc/slider.php
+++ wp-content/themes/sydney/inc/slider.php
@@ -44,43 +44,41 @@ function sydney_slider_template() {
                             <p class="subtitle"><?php echo esc_html(get_theme_mod('slider_subtitle_1','Feel free to look around')); ?></p>
                         </div>
                     </li>
-<!--                     <?php endif; ?>
-                    <?php if ( get_theme_mod('slider_image_2', get_template_directory_uri() . '/images/2.jpg') ) : ?>
+                    <?php endif; ?>
+<!--                     <?php if ( get_theme_mod('slider_image_2', get_template_directory_uri() . '/images/2.jpg') ) : ?>
                     <li>
                         <div class="contain">
                             <h2 class="maintitle"><?php echo esc_html(get_theme_mod('slider_title_2', 'Ready to begin your journey?')); ?></h2>
                             <p class="subtitle"><?php echo esc_html(get_theme_mod('slider_subtitle_2', 'Click the button below')); ?></p>
                         </div>
-                    </li>
-                    <?php endif; ?>
-                    <?php if ( get_theme_mod('slider_image_3') ) : ?>
-                    <li>
-                        <div class="contain">
-                            <h2 class="maintitle"><?php echo esc_html(get_theme_mod('slider_title_3')); ?></h2>
-                            <p class="subtitle"><?php echo esc_html(get_theme_mod('slider_subtitle_3')); ?></p>
-                        </div>
-                    </li>
-                    <?php endif; ?>
-                    <?php if ( get_theme_mod('slider_image_4') ) : ?>
-                    <li>
-                        <div class="contain">
-                            <h2 class="maintitle"><?php echo esc_html(get_theme_mod('slider_title_4')); ?></h2>
-                            <p class="subtitle"><?php echo esc_html(get_theme_mod('slider_subtitle_4')); ?></p>
-                        </div>
-                    </li>
-                    <?php endif; ?>
-                    <?php if ( get_theme_mod('slider_image_5') ) : ?>
-                    <li>
-                        <div class="contain">
-                            <h2 class="maintitle"><?php echo esc_html(get_theme_mod('slider_title_5')); ?></h2>
-                            <p class="subtitle"><?php echo esc_html(get_theme_mod('slider_subtitle_5')); ?></p>
-                        </div>
-                    </li>
-                    <?php endif; ?>   -->                                      
+                    </li> -->
+                    <?php endif; ?>                                       
                 </ul>
             </div>
+<!--             <?php $slider_button = get_theme_mod('slider_button_text', 'Click to begin'); ?>
+            <?php if ($slider_button) : ?>
+                <a href="<?php echo esc_url(get_theme_mod('slider_button_url','#primary')); ?>" class="roll-button button-slider"><?php echo esc_html($slider_button); ?></a>
+            <?php endif; ?> -->
             <div class="custom-widget-content clearfix">
-                <div id="dummy" style="float:left;width:20%">
+                <form class="form-inline" action="action_page.php">
+                    <select name="请选择品牌">
+                        <option value="sony" selected>请选择手机品牌</option>
+                        <option value="sony">索尼</option>
+                        <option value="samsung">三星</option>
+                        <option value="apple">苹果</option>
+                        <option value="htc">HTC</option>
+                    </select>
+                    <select name="请选择故障">
+                        <option value="sony" selected>请选择故障类别</option>
+                        <option value="screen">屏幕</option>
+                        <option value="battery">电池</option>
+                        <option value="headphone_jack">耳机</option>
+                        <option value="camera">相机</option>  
+                    </select>
+                    <input type="submit" Value="查看价格">
+                </form>
+
+<!--                 <div id="dummy" style="float:left;width:20%">
                     <h2></h2>
                 </div>
                 <div id="main_widget_area" class="front-paget-widget-area" style="float:left">
@@ -88,9 +86,8 @@ function sydney_slider_template() {
                 </div>
                 <div id="issue_select_area" class="front-page-issue-select-area" style="float:left">
                     <?php dynamic_sidebar('mywidgetarea-2'); ?>
-                </div>
+                </div> -->
             </div>
-            <?php endif; ?>
         </div>
 
 
