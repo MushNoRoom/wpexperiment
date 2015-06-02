ebe61a7bbae4326422fddf720617162ec272edac
Bryan <fzboybryan@gmail.com>
2015-06-02 01:53:07 -0400

    Update the WP to have use the right password for mysql.

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'Yixiuge1');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '35941876');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
